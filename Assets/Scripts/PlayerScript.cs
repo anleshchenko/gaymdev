9f681c2dd949c3a2426f7798a53ecc09b1fed764
anleshchenko <leandrey19@gmail.com>
2018-12-30 23:24:37 +0200

    movement

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+    private float x, y;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+        Vector2 moveInput = new Vector2(x, y);
+        moveVelocity = moveInput.normalized * speed;
         
     }
+
+    void FixedUpdate() {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+    
 }
