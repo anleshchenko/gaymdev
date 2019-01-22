a757e97eb64b921219078023aac4cd3b80b93aa0
anleshchenko <leandrey19@gmail.com>
2019-01-22 18:12:27 +0200

    animations work now

---
+++ Assets/Player.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public Sprite[] sprites = new Sprite[8];
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+    private float x, y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+        Vector2 moveInput = new Vector2(x, y);
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+        Animate();
+    }
+
+    void Animate()
+    {
+        if (y == 1)
+        {
+            if (x == 1)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = sprites[3];
+            }
+            if (x == -1)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = sprites[1];
+            }
+            if (x == 0)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = sprites[2];
+            }
+        }
+        if (y == -1)
+        {
+            if (x == 1)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = sprites[5];
+            }
+            if (x == -1)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = sprites[7];
+            }
+            if (x == 0)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = sprites[6];
+            }
+        }
+        if (y == 0)
+        {
+            if (x == 1)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = sprites[4];
+            }
+            if (x == -1)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = sprites[0];
+            }
+        }
+    }
+}
