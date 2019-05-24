a0e7e02053d56ebc1601331ccc9e1942d8029945
anleshchenko <leandrey19@gmail.com>
2019-05-24 22:48:57 +0300

    survival new map

---
+++ Assets/Scripts/TreeScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeScript : MonoBehaviour
+{
+    private SpriteRenderer sr;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            sr.color = new Color(1, 1, 1, 0.5f);
+
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            sr.color = new Color(1, 1, 1, 1);
+        }
+    }
+}
