b08a9284525414121733406f33f1bedf921a456d
alexmkcormick <31690046+alexmkcormick@users.noreply.github.com>
2019-06-21 11:52:24 +0300

    Add files via upload

---
+++ Dead.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Dead : MonoBehaviour
+{
+    public GameObject  ButtonRepeat;
+    public GameObject  ButtonBack;
+    
+    public void RepeatScene()
+    {
+        SceneManager.LoadScene("NewSurvival");
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
